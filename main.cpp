ab91191e4d3e180126fdbf9b5da40dddd1f4daba
Statescu Relu <statescu.relu@gmail.com>
2021-10-27 10:30:20 +0300

    update27/10/2021

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include "date.h"
 #include <vector>
 
+using namespace date::literals;
 
 class item{
     /*const*/ int id;
@@ -30,10 +31,10 @@ public:
     }
 
     friend std::ostream &operator<<(std::ostream &os, const item &item){
-        os << "Item ID: " << item.id;
-        os << "Content: " << item.itemContent;
-        os << "Add date: " << item.addDate;
-        os << "Update date: " << item.updateDate;
+        os << "Item ID: " << item.id << "\n";
+        os << "Content: " << item.itemContent << "\n";
+        os << "Add date: " << item.addDate << "\n";
+        os << "Update date: " << item.updateDate << "\n";
     }
 
     ~item() {}
@@ -54,6 +55,22 @@ public:
         item::updateDate = updateDate;
     }
 
+    int getId() const {
+        return id;
+    }
+
+    const std::string &getItemContent() const {
+        return itemContent;
+    }
+
+    const date::year_month_day &getAddDate() const {
+        return addDate;
+    }
+
+    const date::year_month_day &getUpdateDate() const {
+        return updateDate;
+    }
+
 };
 
 class to_do_list{
@@ -62,13 +79,19 @@ class to_do_list{
     int nrOfItems;
 
 public:
+
+    to_do_list(const int id) : id(id) {}
+
     to_do_list(const int id, const std::vector<item> &list) : id(id), list(list) {}
 
     friend std::ostream &operator<<(std::ostream &os, const to_do_list &to_do_list){
-        os << "List ID: " << to_do_list.id;
+        os << "List ID: " << to_do_list.id << "\n";
         for(const auto& item : to_do_list.list)
         {
-
+            os << "Item ID: " << item.getId() << "\n";
+            os << "Content: " << item.getItemContent() << "\n";
+            os << "Add date: " << item.getAddDate() << "\n";
+            os << "Update date: " << item.getUpdateDate() << "\n";
         }
     }
 
@@ -97,7 +120,11 @@ public:
 
 int main(){
 
-    //item i1(1, "ceva", 2021/1/1, 2021/1/1);
-
+    item i1(1, "Ceva", 2021_y/1/1, 2021_y/1/1);
+    item i2(2, "CEVAsiALTCEVA", 2021_y/2/3, 2021_y/1/1);
+    to_do_list l1(1);
+    l1.addItem(i1);
+    l1.addItem(i2);
+    std::cout << l1;
     return 0;
 }
