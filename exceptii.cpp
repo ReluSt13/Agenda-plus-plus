55f969e94d0ca195028951091f9010fceb591f89
Statescu Relu <statescu.relu@gmail.com>
2021-12-12 16:49:55 +0200

    adaugare string literals in exceptii

--- exceptii.cpp
+++ exceptii.cpp
@@ -4,15 +4,17 @@
 
 #include "exceptii.h"
 
-eroare_item::eroare_item(const std::string &arg) : runtime_error("Eroare item: " + arg) {}
+using namespace std::string_literals;
 
-eroare_contentLen::eroare_contentLen(int contentSize) : eroare_item("Eroare nr. caractere: au fost introduse: " + std::to_string(contentSize) + " caractere") {}
+eroare_item::eroare_item(const std::string &arg) : runtime_error("Eroare item: "s + arg) {}
+
+eroare_contentLen::eroare_contentLen(int contentSize) : eroare_item("Eroare nr. caractere: au fost introduse: "s + std::to_string(contentSize) + " caractere"s) {}
 
 eroare_dataUpdate::eroare_dataUpdate() : eroare_item("Eroare data update : data de update nu poate fi inaintea datei de adaugare"){}
 
 
-eroare_lista::eroare_lista(const std::string &arg) : runtime_error("Eroare lista: " + arg) {}
+eroare_lista::eroare_lista(const std::string &arg) : runtime_error("Eroare lista: "s + arg) {}
 
-eroare_numeLista::eroare_numeLista(int nrChar) : eroare_lista("Eroare nume lista: au fost introduse: " + std::to_string(nrChar) + " caractere"){}
+eroare_numeLista::eroare_numeLista(int nrChar) : eroare_lista("Eroare nume lista: au fost introduse: "s + std::to_string(nrChar) + " caractere"s){}
 
-eroare_contentUpdate::eroare_contentUpdate(int contentSize) : eroare_item("Eroare updatare content: au fost introduse: " + std::to_string(contentSize) + " caractere") {}
+eroare_contentUpdate::eroare_contentUpdate(int contentSize) : eroare_item("Eroare updatare content: au fost introduse: "s + std::to_string(contentSize) + " caractere"s) {}
